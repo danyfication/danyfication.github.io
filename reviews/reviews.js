5e3300e2615bc4b321ae4665bd8a6af45afd8b95
Danyfication <danylolacoy@gmail.com>
2026-06-09 14:49:07 +0000

    iwafhiufb

--- reviews/reviews.js
+++ reviews/reviews.js
@@ -11,7 +11,7 @@ const reviewConteudo = {
         score: "9/10",
         image: "../imgs/tboi.png",
         gameHtml: `
-            <div id="tboiTitDiv"><h1 id="tboiTit">The Binding Of Isaac</h1></div>
+            <div id="tboiTitDiv" class="titles"><h1 id="tboiTit">The Binding Of Isaac</h1></div>
             <img id="Img" src="../imgs/tboi.png" alt="The Binding Of Isaac" />
             <h2 id="Nota"><strong>Nota:</strong> 9/10</h2>
             <h3 id="Desc1">Roguelike com ação rápida, itens caóticos e uma curva de dificuldade pesada.</h3>
@@ -28,7 +28,7 @@ const reviewConteudo = {
         score: "10/10",
         image: "../imgs/silksong.png",
         gameHtml: `
-            <div id="silksongTitDiv"><h1 id="silksongTit">Hollow Knight: Silksong</h1></div>
+            <div id="silksongTitDiv" class="titles"><h1 id="silksongTit">Hollow Knight: Silksong</h1></div>
             <img id="Img" src="../imgs/silksong.png" alt="Hollow Knight: Silksong" />
             <h2 id="Nota"><strong>Nota:</strong> 10/10</h2>
             <h3 id="Desc1">Metroidvania com foco em exploração, combate preciso e uma atmosfera única.</h3>
@@ -45,7 +45,7 @@ const reviewConteudo = {
         score: "9/10",
         image: "../imgs/astroneer.png",
         gameHtml: `
-            <div id="astroneerTitDiv"><h1 id="astroneerTit">Astroneer</h1></div>
+            <div id="astroneerTitDiv" class="titles"><h1 id="astroneerTit">Astroneer</h1></div>
             <img id="Img" src="../imgs/astroneer.png" alt="Astroneer" />
             <h2 id="Nota"><strong>Nota:</strong> 9/10</h2>
             <h3 id="Desc1">Jogo de sobrevivência em um ambiente espacial com elementos de construção e exploração.</h3>
@@ -62,7 +62,7 @@ const reviewConteudo = {
         score: "7.5/10",
         image: "../imgs/awaria.png",
         gameHtml: `
-            <div id="awariaTitDiv"><h1 id="awariaTit">Awaria</h1></div>
+            <div id="awariaTitDiv" class="titles"><h1 id="awariaTit">Awaria</h1></div>
             <img id="Img" src="../imgs/awaria.png" alt="Awaria" />
             <h2 id="Nota"><strong>Nota:</strong> 7.5/10</h2>
             <h3 id="Desc1">Jogo de fases curto com alta dificuldade e mecânica de PUDIM.</h3>
@@ -79,7 +79,7 @@ const reviewConteudo = {
         score: "8.5/10",
         image: "../imgs/bopl.png",
         gameHtml: `
-            <div id="boplTitDiv"><h1 id="boplTit">Bopl Battle</h1></div>
+            <div id="boplTitDiv" class="titles"><h1 id="boplTit">Bopl Battle</h1></div>
             <img id="Img" src="../imgs/bopl.png" alt="Bopl Battle" />
             <h2 id="Nota"><strong>Nota:</strong> 8.5/10</h2>
             <h3 id="Desc1">Jogo de disputa entre amigos com alta competitividade e combinações.</h3>
@@ -96,7 +96,7 @@ const reviewConteudo = {
         score: "9/10",
         image: "../imgs/btd6.png",
         gameHtml: `
-            <div id="btd6TitDiv"><h1 id="btd6Tit">Bloon TD 6</h1></div>
+            <div id="btd6TitDiv" class="titles"><h1 id="btd6Tit">Bloon TD 6</h1></div>
             <img id="Img" src="../imgs/btd6.png" alt="Bloon TD 6" />
             <h2 id="Nota"><strong>Nota:</strong> 9/10</h2>
             <h3 id="Desc1">Tower-defense incrível!</h3>
@@ -113,7 +113,7 @@ const reviewConteudo = {
         score: "8/10",
         image: "../imgs/ddlc.png",
         gameHtml: `
-            <div id="ddlcTitDiv"><h1 id="ddlcTit">Doki Doki Literature Club</h1></div>
+            <div id="ddlcTitDiv" class="titles"><h1 id="ddlcTit">Doki Doki Literature Club</h1></div>
             <img id="Img" src="../imgs/ddlc.png" alt="Doki Doki Literature Club" />
             <h2 id="Nota"><strong>Nota:</strong> 8/10</h2>
             <h3 id="Desc1">Web-novel, jogo de escolhas com reviravoltas.</h3>
@@ -130,7 +130,7 @@ const reviewConteudo = {
         score: "9/10",
         image: "../imgs/mr.png",
         gameHtml: `
-            <div id="mrTitDiv"><h1 id="mrTit">Marvel Rivals</h1></div>
+            <div id="mrTitDiv" class="titles"><h1 id="mrTit">Marvel Rivals</h1></div>
             <img id="Img" src="../imgs/mr.png" alt="Marvel Rivals" />
             <h2 id="Nota"><strong>Nota:</strong> 9/10</h2>
             <h3 id="Desc1">Hero-shooter com seus personagens favoritos da Marvel.</h3>
@@ -147,7 +147,7 @@ const reviewConteudo = {
         score: "7/10",
         image: "../imgs/postal.png",
         gameHtml: `
-            <div id="postalTitDiv"><h1 id="postalTit">Postal 1</h1></div>
+            <div id="postalTitDiv" class="titles"><h1 id="postalTit">Postal 1</h1></div>
             <img id="Img" src="../imgs/postal.png" alt="Postal 1" />
             <h2 id="Nota"><strong>Nota:</strong> 7/10</h2>
             <h3 id="Desc1">Shooter visto de cima, bem difícil.</h3>
@@ -164,7 +164,7 @@ const reviewConteudo = {
         score: "8.5/10",
         image: "../imgs/spg.png",
         gameHtml: `
-            <div id="spgTitDiv"><h1 id="spgTit">Slinkie Dinkie</h1></div>
+            <div id="spgTitDiv" class="titles"><h1 id="spgTit">Slinkie Dinkie</h1></div>
             <img id="Img" src="../imgs/spg.png" alt="Slinkie Dinkie" />
             <h2 id="Nota"><strong>Nota:</strong> 8.5/10</h2>
             <h3 id="Desc1">Jogo de bicho virtual, relaxante.</h3>
@@ -181,7 +181,7 @@ const reviewConteudo = {
         score: "8/10",
         image: "../imgs/warframe.png",
         gameHtml: `
-            <div id="warframeTitDiv"><h1 id="warframeTit">Warframe</h1></div>
+            <div id="warframeTitDiv" class="titles"><h1 id="warframeTit">Warframe</h1></div>
             <img id="Img" src="../imgs/warframe.png" alt="Warframe" />
             <h2 id="Nota"><strong>Nota:</strong> 8/10</h2>
             <h3 id="Desc1">RPG e farm, bem satisfatório.</h3>
